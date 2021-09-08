e1f70bcc297b9a46fe45f18cd34dea2c20eb7cac
pallavimalge329 <malgepallavi21@gmail.com>
2021-09-08 14:34:20 +0530

    day 13 updated all

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,7 +1,43 @@
---Day 13
+--Day 13--
+select *
+from HumanResources.Employee;
+
+--Projection--
+select BusinessEntityID, JobTitle
+from HumanResources.Employee;
+
 select *
 from HumanResources.Employee
+where  BusinessEntityID=9;
 
---Projection
-select BusinessEntityID, JobTitle
+
+select *
 from HumanResources.Employee
+where OrganizationLevel=4;
+
+select *
+from HumanResources.Employee
+where SalariedFlag=5;
+
+-- maximum value
+select max(SickLeaveHours)
+from HumanResources.Employee;
+
+select BusinessEntityID,JobTitle,BirthDate,HireDate
+from HumanResources.Employee
+
+select BirthDate
+from HumanResources.Employee
+where BusinessEntityID= 13
+
+select BusinessEntityID 'Employee ID' ,
+JobTitle  as 'Designation',
+BirthDate 'Date of Birth',
+'Joining Date' = HireDate
+from HumanResources.Employee
+
+
+
+
+
+
